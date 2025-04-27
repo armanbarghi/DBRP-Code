82f15d06fa945ded6665eecfbf491cdc6dcbc35f
Arman Barghi <arman.barghi@gmail.com>
2025-04-27 10:52:24 +0330

    fix graph generation bug

--- env/graph_env.py
+++ env/graph_env.py
@@ -37,7 +37,45 @@ def unflatten_pos(flat: int, grid_size: tuple):
 def copy_graph(graph):
 	return graph.clone().detach()
 
-### Random Graph Generation
+def find_target_obj(x, node):
+	i = (x[node, Indices.RELATION] == 1).nonzero(as_tuple=True)[0]
+	if len(i):
+		return i.item()
+	return None
+
+def find_start_obj(x, node):
+	i = (x[:, Indices.RELATION.start + node] == 1).nonzero(as_tuple=True)[0]
+	if len(i):
+		return i.item()
+	return None
+
+def is_empty_object(x, node):
+	return find_start_obj(x, node) is None
+
+def is_edge_in_graph(x, node1, node2):
+	if x[node1, Indices.RELATION.start + node2] == 0:
+		return False
+	return True
+
+def is_stacked_object(x, node):
+	return x[node, Indices.RELATION].sum() > 0
+
+def find_base_obj(x, node):
+	while is_stacked_object(x, node):
+		node = find_target_obj(x, node)
+	return node
+
+def get_obj_pos(x, node):
+	return x[node, Indices.COORD].clone()
+
+def get_obj_size(x, node):
+	return x[node, Indices.SIZE].clone().tolist()
+
+def get_obj_label(x, node):
+	return x[node, Indices.LABEL].item()
+
+def get_obj_relation(x, node):
+	return x[node, Indices.RELATION].clone().tolist()
 
 def in_table_index(coor, size):
     """
@@ -63,6 +101,8 @@ def get_node_poses(num_nodes):
 
     return positions
 
+### Random Graph Generation
+
 def generate_random_coordinates(grid_size: tuple, sizes: list, max_attempts: int=100) -> list:
 	"""
 	Generate random coordinates for objects, restarting if a placement fails.
@@ -82,15 +122,9 @@ def generate_random_coordinates(grid_size: tuple, sizes: list, max_attempts: int
 		coords = []
 
 		for i, size in enumerate(sizes):
-			placed = False
-			attempts = 0
 
-			while not placed:
-				attempts += 1
-				if attempts > max_attempts:
-					# Restart the entire placement process
-					success = False
-					break
+			placed = False
+			for _ in range(max_attempts):
 
 				x_min, x_max = size[0] // 2, grid_size[0] - size[0] // 2 - 1
 				y_min, y_max = size[1] // 2, grid_size[1] - size[1] // 2 - 1
@@ -107,15 +141,18 @@ def generate_random_coordinates(grid_size: tuple, sizes: list, max_attempts: int
 					table[x_slice, y_slice] = i + 1
 					coords.append(coor)
 					placed = True
+					break
 
-			if not success:
+			if not placed:
+				success = False
 				break  # Restart placement process
 
 		if success:
 			return coords  # Successfully placed all objects
+
 	return None  # Failed to place all objects after max attempts
 
-def generate_random_coordinates_with_ratio(grid_size: tuple, sizes: list, ratio: float, max_attempts: int=100, adjustment_factor: float=0.01) -> list:
+def generate_random_coordinates_with_ratio(grid_size: tuple, sizes: list, ratio: float, max_attempts: int=100) -> list:
 	"""
 	Generate random coordinates for objects, ensuring a placement ratio across grid halves.
 
@@ -135,28 +172,17 @@ def generate_random_coordinates_with_ratio(grid_size: tuple, sizes: list, ratio:
 		table = np.zeros(grid_size, dtype=int)
 		success = True
 		coords = []
-		current_ratio = ratio  # Start with the initial ratio
 
 		for i, size in enumerate(sizes):
-			placed = False
-			attempts = 0
 
-			while not placed:
-				attempts += 1
-				if attempts > max_attempts:
-					# Adjust the ratio slightly and restart
-					if current_ratio > 0.5:
-						current_ratio = max(0.5, current_ratio - adjustment_factor)
-					else:
-						current_ratio = min(0.5, current_ratio + adjustment_factor)
-					success = False
-					break
+			placed = False
+			for _ in range(max_attempts):
 
 				# Determine whether to place in the left or right half based on the current ratio
 				x_min, x_max = size[0] // 2, grid_size[0] - size[0] // 2 - 1
 				# y_min, y_max = size[1] // 2, grid_size[1] - size[1] // 2 - 1
 
-				if random.random() < current_ratio:
+				if random.random() < ratio:
 					y_min, y_max = size[1] // 2, grid_size[1] // 2 - 1
 				else:
 					y_min, y_max = grid_size[1] // 2, grid_size[1] - size[1] // 2 - 1
@@ -173,16 +199,19 @@ def generate_random_coordinates_with_ratio(grid_size: tuple, sizes: list, ratio:
 					table[x_slice, y_slice] = i + 1
 					coords.append(coor)
 					placed = True
+					break
 
-			if not success:
-				break  # Restart placement process with adjusted ratio
+			if not placed:
+				success = False
+				break  # Restart placement process
 
 		if success:
 			return coords  # Successfully placed all objects
+
 	return None  # Failed to place all objects after max attempts
 
-def create_graph(num_nodes: int, grid_size: tuple, num_labels: int, object_sizes: list, labels=None, p: float=0.6, ratio: float=0.5, iterations: int=100) -> Data:
-	if iterations == 0:
+def create_graph(num_nodes: int, grid_size: tuple, num_labels: int, object_sizes: dict, labels=None, p: float=0.6, ratio: float=0.5, max_attempts: int=10) -> Data:
+	if max_attempts == 0:
 		raise ValueError('Failed to create a graph with the given parameters')
 	
 	# if grid_size[0] > grid_size[1]:
@@ -197,67 +226,78 @@ def create_graph(num_nodes: int, grid_size: tuple, num_labels: int, object_sizes
 	# max_size_y = max([object_sizes[i][1] for i in range(num_labels)])
 	# if max_size_x > grid_size[0] or max_size_y > grid_size[1]:
 	# 	raise ValueError('Grid size is too small for the objects')
+	assert num_nodes >= 2
 
+	label_flag = True
 	if labels is None:
+		label_flag = False
 		labels = [random.randint(0, num_labels-1) for _ in range(num_nodes)]
-		if np.sum([object_sizes[labels[i]][0]*object_sizes[labels[i]][0] for i in range(num_nodes)]) > grid_size[0]*grid_size[1]:
-			return create_graph(num_nodes, grid_size, num_labels, object_sizes, None, p, iterations-1)
+		if np.sum([object_sizes[labels[i]][0]*object_sizes[labels[i]][1] for i in range(num_nodes)]) > grid_size[0]*grid_size[1]:
+			print('Man! The objects are too big!')
+			return create_graph(num_nodes, grid_size, num_labels, object_sizes, None, p, ratio, max_attempts-1)
 	elif len(labels) != num_nodes:
 		raise ValueError('Number of labels must be equal to the number of nodes')
 	else:
-		if np.sum([object_sizes[labels[i]][0]*object_sizes[labels[i]][0] for i in range(num_nodes)]) > grid_size[0]*grid_size[1]:
-			raise ValueError('objects are too big')
+		if np.sum([object_sizes[labels[i]][0]*object_sizes[labels[i]][1] for i in range(num_nodes)]) > grid_size[0]*grid_size[1]:
+			raise ValueError('Man! The objects are too big!')
 
 	edge_index = torch.tensor([], dtype=torch.long)
 	x_arr = torch.zeros(num_nodes, num_nodes+4)
 	x_arr = torch.cat([torch.tensor(labels, dtype=torch.float).reshape(-1, 1), x_arr], dim=1)
 
-	# Shuffle nodes
-	nodes = [i for i in range(num_nodes)]
-	random.shuffle(nodes)
-
 	# Random label for each node
+	nodes = list(range(num_nodes))
+	random.shuffle(nodes)
 	for node in nodes:
 		x_arr[node][Indices.LABEL] = labels[node]
+		x_arr[node][Indices.SIZE] = torch.tensor(object_sizes[labels[node]], dtype=torch.float32)
 
 	# Random connection between edges
 	for node in nodes:
 		if random.random() < p:
 			node1 = node
-			node2 = random.randint(0, num_nodes-1)
-			if node1 != node2:
-				if torch.any(torch.all(edge_index == torch.tensor([[node2], [node1]], dtype=torch.long), dim=0)):
-					continue
-				if not is_stable(x_arr, node1, node2):
-					continue
-				if torch.sum(x_arr[:, Indices.RELATION.start+node2]) >= 1:
-					continue
-				edge_index = torch.cat([edge_index, torch.tensor([[node1], [node2]], dtype=torch.long)], dim=1)
-				x_arr[node1][Indices.RELATION.start+node2] = 1
+			node2 = node1
+			while node2 == node1:
+				node2 = random.randint(0, num_nodes-1)
+
+			if not is_stable(x_arr, node1, node2):
+				continue
+			if torch.sum(x_arr[:, Indices.RELATION.start + node2]) >= 1:
+				continue
+
+			edge_index = torch.cat([edge_index, torch.tensor([[node1], [node2]], dtype=torch.long)], dim=1)
+			x_arr[node1, Indices.RELATION.start + node2] = 1
 
 	# Allocate random positions to base nodes
-	sizes = [object_sizes[labels[i]] for i in range(num_nodes) if x_arr[i, Indices.RELATION].sum() == 0]
+	base_sizes = [get_obj_size(x_arr, i) for i in range(num_nodes) if not is_stacked_object(x_arr, i)]
+
 	if ratio == 0.5:
-		coords = generate_random_coordinates(grid_size, sizes, 300)
+		coords = generate_random_coordinates(grid_size, base_sizes, 300)
 	else:
-		coords = generate_random_coordinates_with_ratio(grid_size, sizes, ratio, 300)
+		coords = generate_random_coordinates_with_ratio(grid_size, base_sizes, ratio, 300)
+
+	if coords is None:
+		print('Trying again!')
+		if label_flag:
+			return create_graph(num_nodes, grid_size, num_labels, object_sizes, labels, p, ratio, max_attempts-1)
+		else:
+			return create_graph(num_nodes, grid_size, num_labels, object_sizes, None, p, ratio, max_attempts-1)
+	
 	unallocated_nodes = list(range(num_nodes))
 	for i in range(num_nodes):
-		if x_arr[i, Indices.RELATION].sum() == 0:
+		if not is_stacked_object(x_arr, i):
 			x_arr[i][Indices.COORD] = coords.pop(0).clone()
 			unallocated_nodes.remove(i)
 
 	while len(unallocated_nodes) > 0:
 		i = unallocated_nodes.pop(0)
-		if x_arr[i, Indices.RELATION].sum() == 0:
+		if not is_stacked_object(x_arr, i):
 			raise ValueError('Unknown error in creating graph')
-		child_node = (x_arr[i, Indices.RELATION] == 1).nonzero(as_tuple=True)[0].item()
-		if child_node in unallocated_nodes:
+		
+		target_node = find_target_obj(x_arr, i)
+		if target_node in unallocated_nodes:
 			unallocated_nodes.append(i)
 			continue
-		x_arr[i][Indices.COORD] = x_arr[child_node][Indices.COORD].clone()
+		x_arr[i][Indices.COORD] = get_obj_pos(x_arr, target_node)
 
-	for i in range(num_nodes):
-		x_arr[i][Indices.SIZE] = torch.tensor(object_sizes[labels[i]], dtype=torch.float32)
-	
 	return Data(x=x_arr, edge_index=edge_index, pos=get_node_poses(num_nodes))
